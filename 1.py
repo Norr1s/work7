988e450a22ae1e56a9636b6746c4723129f33809
14 <139519101+Norr1s@users.noreply.github.com>
2024-02-27 15:56:17 +0700

    Create 1.py

---
+++ 1.py
@@ -0,0 +1,9 @@
+salary = int (input ())
+if salary < 15000:
+  Print ("ไม่สามารถทำบัตรเครดิดได้")
+elif salary < 70000:
+  print("Silver")
+elif salary < 100000:
+  print("Gold")
+elif salary > 100000:
+  print ("Platinum")
