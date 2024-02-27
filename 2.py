7a56765bd32c50ee8d2c88b44a251bcc9789e381
14 <139519101+Norr1s@users.noreply.github.com>
2024-02-27 15:57:22 +0700

    Create 2.py

---
+++ 2.py
@@ -0,0 +1,11 @@
+score = int (input())
+if score <50:
+  print ("F")
+elif score >=80:
+  print("A")
+elif 70 <= score >= 79:
+  print ("B")
+elif 60 <= score >= 69:
+  print ("C")
+elif 50 <= score >= 59:
+  print ("D")
